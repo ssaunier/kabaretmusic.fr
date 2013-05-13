e0dd0dd5615fcba9dd9be5a9ea8024b24e55a1fb
Sébastien Saunier <sebastien.saunier@gmail.com>
2013-05-13 23:35:41 +0200

    Typo in the deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ cd `dirname $BASH_SOURCE`
 HOST="ftp.kabaretmusic.fr"
 USER=kabaretmusic
 PASSWORD=`security find-generic-password -l "$HOST" -g 2>&1 | grep "password" | cut -d \\" -f 2`
-echo "cd www\nmput .htaccess\nmput *\n" | ftp -i ftp://$USER:$PASSWORD@$HOST
+echo "cd www\nmput .htaccess\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
 echo "cd www\nmkd img\ncd img\nlcd img\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
-echo "cd www\nmkd icomoon\ncd icomoon\nlcd icomoon\nmput style.css\nmkd fonts\nlcd fonts\nmput *\n" | ftp -i ftp://$USER:$PASSWORD@$HOST
+echo "cd www\nmkd icomoon\ncd icomoon\nlcd icomoon\nmput style.css\nmkd fonts\ncd fonts\nlcd fonts\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
