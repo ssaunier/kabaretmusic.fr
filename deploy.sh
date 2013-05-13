895ec7461bcf844d3e18ded486ca1c787caa0fa0
Sébastien Saunier <sebastien.saunier@gmail.com>
2013-05-13 22:28:39 +0200

    Initial version of the site

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 HOST="ftp.kabaretmusic.fr"
 USER=kabaretmusic
 PASSWORD=`security find-internet-password -l "$HOST" -g 2>&1 | grep "password" | cut -d \\" -f 2`
-echo "cd www\nmput .htaccess\nmput  *\nmkd img\ncd img\nlcd img\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
+echo "cd www\nmput .htaccess\nmput *\n" | ftp -i ftp://$USER:$PASSWORD@$HOST
+echo "cd www\nmkd img\ncd img\nlcd img\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
+echo "cd www\nmkd icomoon\ncd icomoon\nlcd icomoon\nmput style.css\nmkd fonts\nlcd fonts\nmput *\n" | ftp -i ftp://$USER:$PASSWORD@$HOST
