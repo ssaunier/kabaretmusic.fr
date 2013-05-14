88abca3dfb41aed30f0de476d4361687e2be4532
Yoann Saunier <yoann.saunier@gmail.com>
2013-05-14 22:01:54 +0200

    login changed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $BASH_SOURCE`
 HOST="ftp.kabaretmusic.fr"
-USER=kabaretmusic
+USER=kabaretm
 PASSWORD=`security find-generic-password -l "$HOST" -g 2>&1 | grep "password" | cut -d \\" -f 2`
 echo "cd www\nmput .htaccess\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
 echo "cd www\nmkd img\ncd img\nlcd img\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
