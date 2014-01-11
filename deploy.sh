675d9de6537c3b2b4754e4c321163b01734f0c02
Sebastien Saunier <seb@saunier.me>
2014-01-11 10:02:44 +0100

    Hosting on GH Pages, not deploying on FTP anymore

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd `dirname $BASH_SOURCE`
-HOST="ftp.kabaretmusic.fr"
-USER=kabaretm
-PASSWORD=`security find-generic-password -l "$HOST" -g 2>&1 | grep "password" | cut -d \\" -f 2`
-echo "cd www\nmput .htaccess\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
-echo "cd www\nmkd img\ncd img\nlcd img\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
-echo "cd www\nmkd icomoon\ncd icomoon\nlcd icomoon\nmput style.css\nmkd fonts\ncd fonts\nlcd fonts\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
